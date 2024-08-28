7b8a29f59d570691957c5004d1c781d000cdc10a
kuku112 <6431501112@lamduan.mfu.ac.th>
2024-08-28 20:36:56 +0700

    ,,,

---
+++ lib/a/cus_success.dart
@@ -0,0 +1,323 @@
+import 'package:flutter/material.dart';
+
+class CusSuccess extends StatefulWidget {
+  const CusSuccess({Key? key}) : super(key: key);
+
+  @override
+  State<CusSuccess> createState() => _CusSuccessState();
+}
+
+class _CusSuccessState extends State<CusSuccess> {
+  final Map<String, String> item = {
+    'name': 'Name Lastname',
+    'to': 'F1',
+    'date': '24/03/24',
+    'gender': 'Male',
+    'price': '50 THB',
+    'comment': 'comment',
+    'image': 'assets/images/profile.png',
+    'status': 'There is a helmet for you.',
+    'status2': 'Bring your own a helmet.',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    // Selecting the first item from listData
+    final Map<String, String> firstItem = item;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Success',
+          style: TextStyle(
+            color: Color(0xFF1A1C43),
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the previous screen
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 5),
+                Image.asset(
+                  firstItem['image'] ?? 'assets/images/profile.png',
+                  width: 50,
+                  height: 50,
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                         '${firstItem['name']} ',
+                              style: const TextStyle(
+                                color: Color(0xFF1A1C43),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 11,
+                              ),
+                            ), 
+                      Icon(
+                      firstItem['gender'] == 'Male' ? Icons.male : Icons.female,
+                      color: const Color(0xFF1A1C43),
+                      size: 15,
+                    ),
+                  ],
+                ),
+                   
+                const SizedBox(height: 5),
+
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Rate ',
+                      style: TextStyle(
+                        color: Color(0xFF1A1C43),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                      size: 15,
+                    ),
+                  ],
+                ),
+               
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.calendar_today,
+                      color: Color(0xFF1A1C43),
+                      size: 15,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      '${firstItem['date']}',
+                      style: const TextStyle(
+                        color: Color(0xFF1A1C43),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.payment,
+                      color: Color(0xFF1A1C43),
+                      size: 15,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      '${firstItem['price']} ',
+                      style: const TextStyle(
+                        color: Color(0xFF1A1C43),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+                
+                const SizedBox(height: 20),
+                Container(
+                  width: 300,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey, // Border color
+                      width: 1, // Border width
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Pickup',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.my_location,
+                              color: Colors.green,
+                              size: 18,
+                            ),
+                            const SizedBox(width: 10),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 5,
+                                horizontal: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.blue[100],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Text(
+                                '${firstItem['from']}',
+                                style: const TextStyle(
+                                  color: Color(0xFF1A1C43),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 11,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 40.0),
+                          child: Text(
+                            '${firstItem['comment']}',
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30.0),
+                          child: Divider(
+                            color: Color(0xFF1A1C43),
+                            thickness: 0.5,
+                            height: 1,
+                            indent: 5,
+                            endIndent: 30,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Drop',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.pin_drop,
+                              color: Color(0xFFD3261A),
+                              size: 18,
+                            ),
+                            const SizedBox(width: 10),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 5,
+                                horizontal: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.blue[100],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Text(
+                                '${firstItem['to']}',
+                                style: const TextStyle(
+                                  color: Color(0xFF1A1C43),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 40.0),
+                          child: Text(
+                            '${firstItem['comment']}',
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30.0),
+                          child: Divider(
+                            color: Color(0xFF1A1C43),
+                            thickness: 0.5,
+                            height: 1,
+                            indent: 5,
+                            endIndent: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/a/tabbarcus/tabbar_cus.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:gotwo_app_user/a/tabbarcus/confirm_tab.dart';
+import 'package:gotwo_app_user/a/tabbarcus/pending_tab.dart';
+import 'package:gotwo_app_user/a/tabbarcus/success_tab.dart';
+import 'package:gotwo_app_user/a/tabbarcus/totravel_tab.dart';
+import 'package:gotwo_app_user/a/tabbarcus/cancel_tab.dart';
+
+class TabbarCus extends StatefulWidget {
+  const TabbarCus({super.key});
+
+  @override
+  State<TabbarCus> createState() => _TabbarCusState();
+}
+
+class _TabbarCusState extends State<TabbarCus> {
+  @override
+  Widget build(BuildContext context) {
+     return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: _backButton(),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 80),
+                      child: Text(
+                        "Status",
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color(0xff1a1c43),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            const TabBar(
+              labelStyle: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
+              labelColor: Color(0xff1a1c43),
+              indicatorColor: Color(0xff1a1c43),
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(
+                  text: "Pending",
+                ),
+                Tab(
+                  text: "Confirm",
+                ),
+                Tab(
+                  text: "ToTravel ",
+                ),
+                Tab(
+                  text: "Success",
+                ),
+                Tab(
+                  text: "Cancel",
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Column(
+                    //Request
+                    children: [
+                      PendingTab(),
+                    ],
+                  ),
+                  Column(
+                    //Confirm
+                    children: [
+                      ConfirmTab(),
+                    ],
+                  ),
+                  Column(
+                    //To travel
+                    children: [
+                      TotravelTab(),
+                    ],
+                  ),
+                  Column(
+                    //Success
+                    children: [
+                      SuccessTab(),
+                    ],
+                  ),
+                  Column(
+                    //Cancel
+                    children: [
+                      CancelTab(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backButton() {
+    return GestureDetector(
+      onTap: () {
+        debugPrint("back");
+      },
+      child: const Icon(
+        Icons.arrow_back_ios,
+        size: 30,
+        color: Color(0xff1a1c43),
+      ),
+    );
+  }
+}
