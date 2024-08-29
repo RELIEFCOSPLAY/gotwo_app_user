7f18c88b13f5f7c19ac850a3485f256d2f1be110
kuku112 <6431501112@lamduan.mfu.ac.th>
2024-08-29 13:57:04 +0700

    เชื่อมหน้าเสดเเร้ว อุอิอิอุอุอุออิอิอิอ

--- lib/a/cus_confirm1.dart
+++ lib/a/cus_confirm1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gotwo_app_user/a/tabbarcus/tabbar_cus.dart';
 
 class CusConfirm extends StatefulWidget {
   const CusConfirm({Key? key}) : super(key: key);
@@ -43,7 +44,10 @@ class _CusConfirmState extends State<CusConfirm> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
-            Navigator.pop(context); // Navigate back to the previous screen
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => (TabbarCus())),
+            );
           },
         ),
       ),
